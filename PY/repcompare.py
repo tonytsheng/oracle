4c61aa59604e21f3d97091b2d043bc69d47f49a3
Tony Sheng <tonytsheng@gmail.com>
2020-08-19 13:43:44 -0400

    Update repcompare.py

--- PY/repcompare.py
+++ PY/repcompare.py
@@ -19,3 +19,19 @@ host = socket.gethostname().split('.',1)[0]
 PrintMsg("HOST: " + host)
 ssirep = "/home/user/dba_arch/scripts/install/ssi/rep." + host
 PrintMsg("SSI_REP: " + ssirep)
+
+sql_rowcount = open("rowcount.sql", w)
+sql_rowcount.write("set serveroutput on;\n")
+sql_rowcount.write("set colsep '|';\n")
+sql_rowcount.write("declare val number;\n")
+sql_rowcount.write("begin\n")
+sql_rowcount.write("  for t in (select table_name from user_tables order by \
+    table_name) loop\n")
+sql_rowcount.write("   execute immediate 'select count(*) from || t.table_name \
+    into val;\n")
+sql_rowcount.write("    dbms_output.put_line (t.table_name || ":" || val);\n")
+sql_rowcount.write("end loop;\n")
+sql_rowcount.write("end;\n")
+sql_rowcount.write("\n")
+sql_rowcount.write("exit\n")
+sql_rowcount.close()
